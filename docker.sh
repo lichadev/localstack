345aad3cc9e97baf882a3bd07efc5189ee6fd043
Lisandro Pintos <licha.pintos@gmail.com>
2024-01-25 21:54:42 -0300

    docker run makes more sense

--- docker.sh
+++ docker.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # pass in the docker username and password as args
+if [ $# -eq 0 ]; then
+  echo "Error: please provide docker username and password as args"
+  exit 1
+fi
+
 if [ $1 = "" ] || [ $2 = "" ]; then
   echo "Error: please provide docker username and password as args"
   exit 1
 fi
+
 # could be through .env files or even using a third language as "middle-man" to pass the credentials
 # for now we will use the args
 DOCKER_USER=$1
@@ -12,25 +18,24 @@ DOCKER_PASS=$2
 
 # check if docker is installed
 function docker_check {
-  if [ ! -x "$(command -v docker)" ]; then
-    echo 'Error: docker is not installed.' >&2
-    exit 1
-  fi
-  #log into a docker account
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker pull localstack/localstack:latest
-  if [ ! $? -eq 0 ]; then
-    echo "Error: docker image localstack/localstack:latest not found"
-    exit 1
-  fi
-  docker create container -p 4566:4566 -p 4510-4559:4510 -p 8080:8080 --name localstack localstack/localstack
-  docker start localstack
-  # check if localstack is running
-  docker ps | grep localstack
-  if [ ! $? -eq 0 ]; then
-    echo "Error: localstack container not running"
-    exit 1
-  fi
+if ! docker info >/dev/null 2>&1; then
+  echo "Error: Docker is not installed or not running."
+  exit 1
+fi
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+# pull localStack image
+docker pull localstack/localstack:$TAG
+
+# start localstack container
+docker run -d --name localstack \
+  -p 4566:4566 -p 4510-4559:4510 -p 8080:8080 \
+  localstack/localstack
+
+# check if localstack container is running
+if ! docker inspect -f '{{.State.Running}}' localstack | grep true; then
+  echo "Error: Localstack container not running"
+  exit 1  
+fi
 }
 
 # This could be other option if we want to use terraform via a python script
